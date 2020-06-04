b4e8851cb4359084b8084f6ac1c417607a3fe677
Tony-Kara <eniolaanthony@gmail.com>
2020-06-04 16:02:41 +0600

    Initial Commit

---
+++ Age and multicultural appearance Detector - Ooh Mama App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Age and multicultural appearance Detector - Ooh Mama App
+//
+//  Created by mac on 6/4/20.
+//  Copyright © 2020 EniTony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
